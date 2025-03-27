215be3427bf8269ab27a6835bfc2f3760e911add
Rik Huijzer <github@huijzer.xyz>
2025-03-27 08:52:54 +0100

    Update

--- src/lm.rs
+++ src/lm.rs
@@ -2,6 +2,9 @@ use faer_ext::*;
 use ndarray::Array1;
 use ndarray::Array2;
 use ndarray::Axis;
+use faer::prelude::*;
+use faer::linalg::solvers::SolveLstsq;
+use faer::linalg::solvers::SolveLstsqCore;
 
 struct PreprocessedData {
     xs: Array2<f64>,
@@ -113,38 +116,37 @@ pub struct LinearRegression {
 pub struct LsqsqResult {
     pub coef: Array1<f64>,
     pub residuals: Array1<f64>,
-    pub ys: Array1<f64>,
 }
 
 fn lstsq(xs: &Array2<f64>, ys: &Array1<f64>) -> LsqsqResult {
     let xs_f = xs.view().into_faer();
-    let svd = xs_f.svd().expect("SVD failed");
-    let u = svd.U().into_ndarray();
-    let s = svd.S();
-    let v = svd.V().into_ndarray();
+    // Create a copy for QR decomposition
+    let xs_qr = xs_f.cloned();
+    // Create a mutable copy for the solution
+    let solution = ys.clone().into_shape_clone((ys.dim(), 1));
+    let solution = solution.expect("Failed to clone ys");
+    let mut solution_f = solution.view().into_faer().to_owned();
+    let solution_mut = solution_f.as_mut();
+    
+    // We don't have to add a 1s column because the data was already centered.
+    
+    // Perform QR decomposition and solve the least squares problem
+    let qr = xs_qr.qr();
+    let conj = faer::Conj::No;
+    qr.solve_lstsq_in_place_with_conj(conj, solution_mut);
+    // let x = xs_f.as_ref().subrows(0, xs.nrows());
+    let coef = solution_f.subrows(0, xs.nrows());
+    let coef = coef.into_ndarray();
+    let coef = coef.to_owned();
+    tracing::info!("coef: {:?}", &coef);
+    let ys2 = ys.clone();
+    let coef = Array1::from_iter(coef.into_iter());
+    let residuals = ys2 - &xs_f.into_ndarray().dot(&coef);
 
-    // Compute the pseudo-inverse solution.
-    let mut s_inv = Array2::<f64>::zeros((s.dim(), s.dim()));
-    for i in 0..s.dim() {
-        let val = s[i];
-        if val > 1e-10 {
-            s_inv[(i, i)] = 1.0 / val;
-        }
-    }
-
-    // Compute coefficients: V * S⁻¹ * U^T * y
-    let uty = u.t().dot(ys);
-    let uty = &uty.as_slice().unwrap()[..s_inv.nrows()];
-    let uty = Array1::from_shape_vec((s_inv.nrows(),), uty.to_vec()).unwrap();
-    let s_inv_uty = s_inv.dot(&uty);
-    let coef = v.t().dot(&s_inv_uty);
-
-    let residuals = ys - &xs_f.into_ndarray().dot(&coef);
-
+    tracing::info!("xs_f: {:?}", &xs_f.into_ndarray());
     LsqsqResult {
         coef,
         residuals,
-        ys: ys.clone(),
     }
 }
 
